86f6d06dc6b872285e5193465ccb0ddb08c4137f
Arthur5492 <arthurofr@gmail.com>
2024-10-15 22:26:07 -0300

    Adicionado logica para 2 solenoides(inferior e superior)

--- src/waterLevel.h
+++ src/waterLevel.h
@@ -5,17 +5,30 @@
 
 namespace waterLevel
 {
-  size_t PIN = 13;
-  void setup()
+  uint8_t pin_topLevel;
+  uint8_t pin_bottomLevel;
+  void setup(uint8_t PIN1,uint8_t PIN2)
   {
-    pinMode(PIN, INPUT);
+    pin_topLevel =    PIN1;
+    pin_bottomLevel = PIN2;
+
+    pinMode(pin_topLevel, INPUT);
+    pinMode(pin_bottomLevel, INPUT);
   }
 
-  bool isHIGH()
+  /// @brief  Retorna true se o sensor inferior estiver false
+  bool isLow()
   {
-    return digitalRead(PIN) == HIGH;
+    return digitalRead(pin_bottomLevel) == LOW;
   }
-  
+
+
+  /// @brief Retorna true se  o sensor superior estiver cheio
+  bool isFull()
+  {
+    return digitalRead(pin_topLevel) == HIGH; 
+  }
+
 }
 
 
