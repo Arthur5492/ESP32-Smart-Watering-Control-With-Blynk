6ef61164fcfbc8483dbb21a25592d8bb4980638b
Arthur5492 <arthurofr@gmail.com>
2024-10-15 22:25:45 -0300

    Logica Solenoide adicionada

---
+++ src/solenoide.h
@@ -0,0 +1,33 @@
+#ifndef SOLENOIDE_H
+#define SOLENOIDE_H
+
+#include <Arduino.h>
+
+namespace solenoide
+{
+  uint8_t _pin;
+  bool actualState;
+
+  void setup(uint8_t PIN)
+  {
+    _pin = PIN;
+    pinMode(_pin,OUTPUT);
+  };
+  
+
+  void turnOn()
+  {
+    digitalWrite(_pin,HIGH);
+    actualState = HIGH;
+    Serial.print("Solenoide ativada \n");
+  }
+
+  void turnOff()
+  {
+    digitalWrite(_pin,LOW);
+    actualState = LOW;
+    Serial.print("Solenoide Desativada \n");
+  }
+}
+
+#endif
