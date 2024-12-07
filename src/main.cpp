12017c43b4bc81a0cf1c107835928a329fd98eff
Arthur5492 <arthurofr@gmail.com>
2024-12-07 14:38:00 -0300

    V1 Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,64 @@
+//Insert your Template ID and Template Name
 #include "wifiSetup.h"
-
-
-//SENSORS
-#include "dht11.h"
 #include "waterTankManager.h"
-//END SENSORS
+#include "irrigation.h"
+#include "blynkHandler.h"
 
+unsigned long timer = 0;
+#define BOOT_BUTTON 0
 void setup() { 
   Serial.begin(9600);
+  pinMode(2,OUTPUT); //Built-in LED
+  pinMode(BOOT_BUTTON,INPUT_PULLUP); //BOOT BUTTON
+
+  //PINS
+  waterTank.setWaterLevelPins(18,19);
+  soilMoisture.setPIN(34);
+  dht11.setPIN(33);
+  waterTank.setSolenoidPin(14);
+  irrigationSystem.setPumpPIN(27);
+  //ENDPINS
+
+  //PARAMETERS
+  irrigationSystem.setSoilMostureMin(5);
+  irrigationSystem.setSoilMostureMax(15);
+  irrigationSystem.setHumThreshold(35);
+  irrigationSystem.setTempThreshold(25);
+  //END PARAMETERS
+
+  wifiSetup.begin();
   
-  ///      PINS SETUP      ///
-  waterTank.setPINS(27,26,25);
-  dht11.setPIN(22);
-  //? waterPump, soil_mosture
-  ///      PINS SETUP      ///
-
-  acessPoint.begin();
-
   if(WiFi.isConnected()) 
     blynkHandler.config();
+
 }
 
 void loop() {
-  //Acess Point to connect to wifi by captive portal
-  acessPoint.loop(); 
   
-  //Blynk will not always be connected, and we need the logic always running regardless of Blynk
-  if(Blynk.connected()) 
-  {
-    Blynk.run();
-    blynkHandler.publishData();  
-  }
-  
-  waterTank.update();
+  wifiSetup.loop(); 
 
-  delay(100);
+  if(Blynk.connected()) 
+    blynkHandler.publishData();
+  
+  if(millis() - timer >= 2000) //Simpliest Timer to reduce amount of checks per iteration
+  {
+    waterTank.update();
+    irrigationSystem.run();
+    timer = millis();
+
+    // Serial.printf("Sensor Topo: %d\n",waterTank.get_topSensor());
+    // Serial.printf("Sensor Fundo: %d\n",waterTank.get_botSensor());
+    // Serial.printf("Estado do Tanque: %s\n", waterTank.getStringTankState());
+    // Serial.printf("Estado irrigacao: %d\n",irrigationSystem.getIsIrrigating());
+    // Serial.printf("Temperatura: %d\n", dht11.getTemperature());
+    // Serial.printf("Humidade Solo: %d\n",soilMoisture.getPercentValue());
+  }
+
+  if(digitalRead(BOOT_BUTTON) == LOW)
+  {
+    wifiSetup.clearNVS();
+    esp_restart();
+  }
+
+  
 }
